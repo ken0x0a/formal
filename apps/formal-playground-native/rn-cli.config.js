ee419695d64e4c10f5893e2a4e8063889772a8bf
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-06-23 14:43:43 +0900

    chore: remove unused patches

--- apps/formal-playground-native/rn-cli.config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, @typescript-eslint/no-var-requires */
-const {
-  createReactNativeConfiguration,
-} = require('@kevinwolf/expo-yarn-workspaces')
-
-module.exports = createReactNativeConfiguration(__dirname)
